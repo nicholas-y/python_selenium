79fea71a36db1d50e2f27e1ec490b9af20014b39
nikolai-y <nikolai.yasiukevich@gmail.com>
2018-02-04 15:51:17 +0300

    add group class. Use class in method's parameters initialization

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from group import Group
+
 
 def is_alert_present(wd):
     try:
@@ -9,39 +11,39 @@ def is_alert_present(wd):
     except:
         return False
 
+
 class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False}, firefox_binary="c:/Program Files/Mozilla Firefox/firefox.exe")
+        self.wd = WebDriver(capabilities={"marionette": False},
+                            firefox_binary="c:/Program Files/Mozilla Firefox/firefox.exe")
         self.wd.implicitly_wait(60)
-    
+
     def test_test_add_group(self):
-        success = True
         wd = self.wd
         self.open_login_page(wd)
-        self.do_login(wd)
+        self.do_login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_new_group(wd)
+        self.create_new_group(wd, Group(name="groupA", header="group header", footer="group footer"))
         self.logout(wd)
-        self.assertTrue(success)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_new_group(self, wd):
+    def create_new_group(self, wd, group):
         # open create group page
         wd.find_element_by_name("new").click()
         # enter group name
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("groupA")
+        wd.find_element_by_name("group_name").send_keys(group.name)
         # enter group header
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("group header")
+        wd.find_element_by_name("group_header").send_keys(group.header)
         # enter group footer
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("group footer")
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # click submit button
         wd.find_element_by_name("submit").click()
         # open group list page
@@ -50,15 +52,15 @@ class test_add_group(unittest.TestCase):
     def open_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def do_login(self, wd):
+    def do_login(self, wd, username, password):
         # enter username
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         # enter password
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         # click login button
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
@@ -67,6 +69,7 @@ class test_add_group(unittest.TestCase):
 
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
