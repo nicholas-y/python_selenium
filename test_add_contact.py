02a04227e5caf1edba4bf07577ee29a11ecc7255
nikolai-y <nikolai.yasiukevich@gmail.com>
2018-02-09 19:07:29 +0300

    move to pytest

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,32 +1,21 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from contact import Contact
 from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-    
-    def test_test_add_contact(self):
-        self.app.do_login(username="admin", password="secret")
-        self.app.create_new_contact(Contact(firstname="Tom", middlename="J", lastname="Tomson", nickname="Tm" ,
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_test_add_contact(app):
+    app.do_login(username="admin", password="secret")
+    app.create_new_contact(Contact(firstname="Tom", middlename="J", lastname="Tomson", nickname="Tm" ,
                                         title="Boss", company="Comp", address="address line", homephone="800-900",
                                         email="auto@test.com", birth_year="1970",
                                         secondary_address="address line secondary",
                                         secondary_homephone="801-901", notes="contact record"))
-        self.app.do_logout()
-
-
-
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == '__main__':
-    unittest.main()
+    app.do_logout()
--- test_add_group.py
+++ test_add_group.py
@@ -1,29 +1,17 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from group import Group
 from application import Application
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_test_add_group(self):
-        self.app.do_login(username="admin", password="secret")
-        self.app.create_new_group(Group(name="groupA", header="group header", footer="group footer"))
-        self.app.do_logout()
-
-    def tearDown(self):
-        self.app.destroy()
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_test_add_group(app):
+    app.do_login(username="admin", password="secret")
+    app.create_new_group(Group(name="groupA", header="group header", footer="group footer"))
+    app.do_logout()
