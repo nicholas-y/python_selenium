842ad511839be324429cdce0808e5ca97914a2a1
nikolai-y <nikolai.yasiukevich@gmail.com>
2018-02-21 22:35:42 +0300

    add groups list compare

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,4 +3,7 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="groupA", header="group header", footer="group footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -6,10 +6,16 @@ def test_edit_group_name(app):
     i = random.randint(1, 25)
     if app.group.count() == 0:
         app.group.create(Group(name="Test Group"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first(Group(name="groupA" + str(i)))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_edit_group_header(app):
     i = random.randint(1, 25)
     if app.group.count() == 0:
         app.group.create(Group(name="Test Group"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first(Group(header="New header" + str(i)))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
