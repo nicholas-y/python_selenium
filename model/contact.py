04e4a92ae5151c92a17f5cbe4c48196d24a95a21
nikolai-y <nikolai.yasiukevich@gmail.com>
2018-03-04 13:48:53 +0300

    add random parameters generation for create group, contact tests

--- model/contact.py
+++ model/contact.py
@@ -29,7 +29,7 @@ class Contact:
         self.all_emails = all_emails
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.lastname, self.firstname)
+        return "%s:%s:%s:%s" % (self.id, self.lastname, self.firstname, self.address)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id)\
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*5
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", lastname="", address="")] + [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 15), address=random_string("address", 20))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Tom", middlename="J", lastname="Tomson", nickname="Tm",
-                               title="Boss", company="Comp", address="address line", homephone="800-900",
-                               email="auto@test.com", birth_year="1970",
-                               secondary_address="address line secondary",
-                               secondary_homephone="801-901", notes="contact record")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contact_list = app.contact.get_contact_list()
