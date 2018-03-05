1461859823e1c0ac249545a83869f0b3e480f62e
nikolai-y <nikolai.yasiukevich@gmail.com>
2018-03-05 13:06:24 +0300

    add browser and baseUrl as command line run options

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -6,13 +6,22 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False},
-                            firefox_binary="c:/Program Files/Mozilla Firefox/firefox.exe")
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(capabilities={"marionette": False},
+                                        firefox_binary="c:/Program Files/Mozilla Firefox/firefox.exe")
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -24,7 +33,7 @@ class Application:
     def open_login_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_css_selector("input[type=\"submit\"]")) > 0):
-            wd.get("http://localhost:8080/addressbook/")
+            wd.get(self.base_url)
 
     def open_home_page(self):
         wd = self.wd
