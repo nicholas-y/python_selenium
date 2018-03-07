ab47564cba0644fae006f91f5ea0e216f92de919
nikolai-y <nikolai.yasiukevich@gmail.com>
2018-03-07 18:06:30 +0300

    store settings in config file, add command line option with path to this file

--- conftest.py
+++ conftest.py
@@ -1,19 +1,25 @@
 import pytest
 from fixture.application import Application
+import json
+import os
 
 fixture = None
+target = None
+
 
 @pytest.fixture()
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        target_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                        request.config.getoption("--target"))
+        with open(target_file_path) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target["baseUrl"])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -27,5 +33,5 @@ def stop(request):
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost:8080/addressbook/")
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--target", action="store", default="target.json")
