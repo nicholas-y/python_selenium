e38bbde7b3e1735ec2653e66980505b7c413b5ac
nikolai-y <nikolai.yasiukevich@gmail.com>
2018-03-11 21:04:13 +0300

    add test data loader from data class

--- data/contact.py
+++ data/contact.py
@@ -1,7 +1,7 @@
 from model.contact import Contact
 
 
-constant = [
+testdata = [
     Contact(firstname="name1", lastname="lastname1", address="address1"),
     Contact(firstname="name2", lastname="lastname2", address="address")
 ]
