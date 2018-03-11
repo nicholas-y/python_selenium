3a7e6a159b302945fc84c69c3f9d2fd21453eaef
nikolai-y <nikolai.yasiukevich@gmail.com>
2018-03-11 21:25:55 +0300

    read test data from json file

--- generator/contact.py
+++ generator/contact.py
@@ -14,7 +14,7 @@ except getopt.GetoptError as err:
 
 
 n = 5
-f = "data/contacts.json"
+f = "data/contact.json"
 
 for o, a in opts:
     if o == "-n":
@@ -37,4 +37,4 @@ file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
     jsonpickle.set_encoder_options('json', indent=2)
-    out.write(jsonpickle.encode(testdata, unpicklable=False))
+    out.write(jsonpickle.encode(testdata))
