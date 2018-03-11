459a3e49b855de96aba87a4d69bede1654bf15b1
nikolai-y <nikolai.yasiukevich@gmail.com>
2018-03-11 16:06:37 +0300

    Add test data generators. Save data to json file using jsonpickle

---
+++ data/contact.py
@@ -0,0 +1,9 @@
+from model.contact import Contact
+
+
+constant = [
+    Contact(firstname="name1", lastname="lastname1", address="address1"),
+    Contact(firstname="name2", lastname="lastname2", address="address")
+]
+
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import pytest
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*5
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", lastname="", address="")] + [
-    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 15), address=random_string("address", 20))
-    for i in range(5)
-]
+from data.contact import constant as testdata
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
